9cee0b584c9fe9fbc1d3a58b46718bb7b5d3781d
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-26 18:58:02 -0700

    set env config

---
+++ apps/server/env.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import "dotenv/config";
+
+const envSchema = z.object({
+  PORT: z.string().default("8080"),
+  CORS_ORIGIN: z.string().default("http://localhost:5173"),
+  REDIS_URL: z.string(),
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.string(),
+  REDIS_USERNAME: z.string(),
+  REDIS_PASSWORD: z.string(),
+  MONGODB_USER: z.string(),
+  MONGODB_PASSWORD: z.string(),
+  MONGODB_HOST: z.string(),
+  MONGODB_DB: z.string(),
+  MONGODB_APPNAME: z.string(),
+  FIREBASE_SERVICE_ACCOUNT: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_S3_BUCKET_NAME: z.string(),
+  AWS_S3_BUCKET_REGION: z.string(),
+});
+
+export const env = envSchema.parse({
+  ...process.env,
+});
