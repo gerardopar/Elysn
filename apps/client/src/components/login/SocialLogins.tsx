eb62c008527a3d89e01ec099a2d976baa262370e
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-28 00:19:24 -0700

    initialize firebase

--- apps/client/src/components/login/SocialLogins.tsx
+++ apps/client/src/components/login/SocialLogins.tsx
@@ -3,13 +3,17 @@ import React from "react";
 import GoogleIcon from "@components/svgs/GoogleIcon";
 import AppleIcon from "@components/svgs/AppleIcon";
 
+import { useFirebase } from "../../hooks/useFirebase";
+
 export const SocialLogins: React.FC = () => {
+  const { handleSignInWithGoogle } = useFirebase();
+
   const buttonStyles = `text-center flex items-center justify-center gap-2 text-seasalt w-full 
       border border-seasalt rounded-[12px] px-4 py-2 max-w-[375px]`;
 
   return (
     <div className="w-full flex justify-center items-center flex-col gap-2">
-      <button className={buttonStyles}>
+      <button onClick={handleSignInWithGoogle} className={buttonStyles}>
         <GoogleIcon className="h-6 w-6" /> Google Login
       </button>
       <button className={buttonStyles}>
