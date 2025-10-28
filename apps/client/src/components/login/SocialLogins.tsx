137dfda3550b808e8df24ce09650673bb2823247
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-28 00:13:04 -0700

    add social login buttons

---
+++ apps/client/src/components/login/SocialLogins.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import GoogleIcon from "@components/svgs/GoogleIcon";
+import AppleIcon from "@components/svgs/AppleIcon";
+
+export const SocialLogins: React.FC = () => {
+  const buttonStyles = `text-center flex items-center justify-center gap-2 text-seasalt w-full 
+      border border-seasalt rounded-[12px] px-4 py-2 max-w-[375px]`;
+
+  return (
+    <div className="w-full flex justify-center items-center flex-col gap-2">
+      <button className={buttonStyles}>
+        <GoogleIcon className="h-6 w-6" /> Google Login
+      </button>
+      <button className={buttonStyles}>
+        <AppleIcon className="h-6 w-6" /> Apple Login
+      </button>
+    </div>
+  );
+};
+
+export default SocialLogins;
