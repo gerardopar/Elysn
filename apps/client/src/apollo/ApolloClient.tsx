a990cb6d1e6ad58ab52ceb66ddf2506116101c69
Gerardo Paredes <gerardparedes23@gmail.com>
2025-11-01 15:40:03 -0700

    configure apollo client

---
+++ apps/client/src/apollo/ApolloClient.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useMemo, useState } from "react";
+import { ApolloProvider } from "@apollo/client/react";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { SetContextLink } from "@apollo/client/link/context";
+import { getAuth } from "firebase/auth";
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000/" });
+
+export const useApolloClient = () => {
+  const auth = getAuth();
+  const [ready, setReady] = useState(false);
+
+  const authLink = useMemo(
+    () =>
+      new SetContextLink(async (prevCtx) => {
+        const user = auth.currentUser;
+        const token = user ? await user.getIdToken() : "";
+        return {
+          headers: {
+            ...prevCtx.headers,
+            authorization: token ? `Bearer ${token}` : "",
+          },
+        };
+      }),
+    [auth]
+  );
+
+  const apolloClient = useMemo(
+    () =>
+      new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+      }),
+    [authLink]
+  );
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(() => {
+      setReady(true);
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  return { apolloClient, ready };
+};
+
+export const ApolloClientProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { apolloClient, ready } = useApolloClient();
+  if (!ready) return null;
+
+  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
+};
+
+export default ApolloClientProvider;
