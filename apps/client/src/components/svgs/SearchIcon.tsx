e81aad1ed8969388792deffafe9a3f6f49986705
Gerardo Paredes <gerardparedes23@gmail.com>
2025-11-03 23:02:27 -0800

    add ion icons

--- apps/client/src/components/svgs/SearchIcon.tsx
+++ apps/client/src/components/svgs/SearchIcon.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 
 import { IonIcon } from "@ionic/react";
+import { search, searchOutline, searchSharp } from "ionicons/icons";
 
-const SearchIcon: React.FC<{
-  className?: string;
-  variant?: "search" | "search-outline" | "search-sharp";
-}> = ({ className, variant = "search-outline" }) => {
-  return <IonIcon name={variant} className={className} />;
+import { type IonIconProps } from "./ionic-icons.helpers";
+
+const SearchIcon: React.FC<
+  {
+    className?: string;
+    variant?: "solid" | "outline" | "sharp";
+  } & IonIconProps
+> = ({ className, variant = "outline", ...props }) => {
+  switch (variant) {
+    case "solid":
+      return <IonIcon icon={search} className={className} {...props} />;
+    case "sharp":
+      return <IonIcon icon={searchSharp} className={className} {...props} />;
+    default:
+      return <IonIcon icon={searchOutline} className={className} {...props} />;
+  }
 };
 
 export default SearchIcon;
---
+++ apps/client/src/components/svgs/ionic-icons.helpers.ts
@@ -0,0 +1,12 @@
+export type IonIconProps = {
+  color?: string;
+  flipRtl?: boolean;
+  icon?: string;
+  ios?: string;
+  lazy?: boolean;
+  md?: string;
+  mode?: "ios" | "md";
+  name?: string;
+  size?: string;
+  src?: string;
+};
