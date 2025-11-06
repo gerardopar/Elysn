0f271d1ee62023ca26eb34543dcda9fcecb0fadd
Gerardo Paredes <gerardparedes23@gmail.com>
2025-11-05 22:11:26 -0800

    add user avatar, info

--- apps/client/src/components/user/UserAvatar.tsx
+++ apps/client/src/components/user/UserAvatar.tsx
@@ -7,13 +7,14 @@ import { useCurrentUser } from "@stores/user";
 export const UserAvatar: React.FC<{ className?: string }> = ({ className }) => {
   const { user } = useCurrentUser();
 
-  const src =
-    "https://wallpapers-clan.com/wp-content/uploads/2025/04/batman-logo-black-background-desktop-wallpaper-preview.jpg";
+  console.log(user);
+
+  const src = user?.photoURL;
 
   return (
     <IonAvatar className={className}>
       <img
-        src={src}
+        src={src || ""}
         alt={"User avatar"}
         className="w-full h-full object-cover"
       />
---
+++ apps/client/src/components/user/UserInfo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { IonRow } from "@ionic/react";
+
+import { useCurrentUser } from "@stores/user";
+
+export const UserInfo: React.FC = () => {
+  const { user } = useCurrentUser();
+
+  return (
+    <IonRow className="flex flex-col">
+      <span className="text-primary-light font-roboto text-sm">
+        {user?.displayName}
+      </span>
+      <span className="text-gray-400 font-roboto text-xs">Beta</span>
+    </IonRow>
+  );
+};
+
+export default UserInfo;
