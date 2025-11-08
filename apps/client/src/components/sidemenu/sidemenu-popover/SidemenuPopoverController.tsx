069791b88e45e80f4395fb3de609643de6ae74f3
Gerardo Paredes <gerardparedes23@gmail.com>
2025-11-07 16:59:31 -0800

    add sheet modal

--- apps/client/src/components/sidemenu/sidemenu-popover/SidemenuPopoverController.tsx
+++ apps/client/src/components/sidemenu/sidemenu-popover/SidemenuPopoverController.tsx
@@ -1,32 +1,54 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-import { IonRow, useIonPopover } from "@ionic/react";
-
+import {
+  IonRow,
+  useIonPopover,
+  useIonModal,
+  IonMenuToggle,
+} from "@ionic/react";
 import SidemenuPopover from "./SidemenuPopover";
 import UserAvatar from "../../user/UserAvatar";
 import UserInfo from "../../user/UserInfo";
 
+import useDeviceWidth from "@hooks/useDeviceWidth";
+
 export const SidemenuPopoverController: React.FC = () => {
   const history = useHistory();
+  const { isMobile } = useDeviceWidth();
 
   const [present, dismiss] = useIonPopover(SidemenuPopover, {
     dismiss: () => dismiss(),
     history: history,
   });
 
-  return (
+  const [presentModal, dismissModal] = useIonModal(SidemenuPopover, {
+    dismiss: () => dismissModal(),
+    history: history,
+  });
+
+  const handlePopover = (e: React.MouseEvent) => {
+    if (isMobile) {
+      presentModal({
+        initialBreakpoint: 1,
+        breakpoints: [0, 1],
+        cssClass: "sidemenu-modal",
+      });
+    } else {
+      present({
+        event: e.nativeEvent,
+        side: "top",
+        alignment: "center",
+        showBackdrop: false,
+        cssClass: "sidemenu-popover",
+      });
+    }
+  };
+
+  const popoverController = (
     <button
       id="open-popover"
-      onClick={(e) =>
-        present({
-          event: e.nativeEvent,
-          side: "top",
-          alignment: "center",
-          showBackdrop: false,
-          cssClass: "sidemenu-popover",
-        })
-      }
+      onClick={(e) => handlePopover(e)}
       className="w-full"
     >
       <IonRow className="w-full flex items-center justify-start gap-2 rounded-lg px-2 py-2">
@@ -35,6 +57,12 @@ export const SidemenuPopoverController: React.FC = () => {
       </IonRow>
     </button>
   );
+
+  if (isMobile) {
+    return <IonMenuToggle menu="main">{popoverController}</IonMenuToggle>;
+  }
+
+  return popoverController;
 };
 
 export default SidemenuPopoverController;
