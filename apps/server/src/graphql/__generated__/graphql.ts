992b0ed0928aa08418e682ca9342bdb022fc1424
Gerardo Paredes <gerardparedes23@gmail.com>
2025-11-15 15:13:06 -0800

    add persona schema

--- apps/server/src/graphql/__generated__/graphql.ts
+++ apps/server/src/graphql/__generated__/graphql.ts
@@ -21,6 +21,8 @@ export type Chat = {
   __typename?: 'Chat';
   createdAt: Scalars['Float']['output'];
   id: Scalars['ID']['output'];
+  personaId: Scalars['ID']['output'];
+  summary?: Maybe<Scalars['String']['output']>;
   title?: Maybe<Scalars['String']['output']>;
   topic?: Maybe<Scalars['String']['output']>;
   updatedAt: Scalars['Float']['output'];
@@ -51,7 +53,7 @@ export type Message = {
   __typename?: 'Message';
   chatId?: Maybe<Scalars['ID']['output']>;
   id: Scalars['ID']['output'];
-  metadata?: Maybe<Scalars['JSON']['output']>;
+  metadata?: Maybe<MessageMetadata>;
   sender: MessageSenderEnum;
   text: Scalars['String']['output'];
   timestamp: Scalars['Float']['output'];
@@ -60,10 +62,18 @@ export type Message = {
 
 export type MessageInput = {
   chatId?: InputMaybe<Scalars['ID']['input']>;
-  metadata?: InputMaybe<Scalars['JSON']['input']>;
+  metadata?: InputMaybe<MessageMetadata>;
   sender: MessageSenderEnum;
   text: Scalars['String']['input'];
   timestamp: Scalars['Float']['input'];
+};
+
+export type MessageMetadata = {
+  __typename?: 'MessageMetadata';
+  emotion?: Maybe<Scalars['String']['output']>;
+  intent?: Maybe<Scalars['String']['output']>;
+  isMemoryWorthy?: Maybe<Scalars['Boolean']['output']>;
+  memoryTag?: Maybe<Scalars['String']['output']>;
 };
 
 export enum MessageSenderEnum {
@@ -81,6 +91,7 @@ export type Mutation = {
   deleteMessage: Message;
   deleteUser: User;
   updateMessage: Message;
+  updatePersona: Persona;
   updateUser: User;
   upsertUser: User;
 };
@@ -125,6 +136,12 @@ export type MutationUpdateMessageArgs = {
 };
 
 
+export type MutationUpdatePersonaArgs = {
+  id: Scalars['ID']['input'];
+  input: UpdatePersonaInput;
+};
+
+
 export type MutationUpdateUserArgs = {
   id: Scalars['ID']['input'];
   name?: InputMaybe<Scalars['String']['input']>;
@@ -139,6 +156,72 @@ export type MutationUpsertUserArgs = {
   picture?: InputMaybe<Scalars['String']['input']>;
 };
 
+export type Persona = {
+  __typename?: 'Persona';
+  avatarUrl?: Maybe<Scalars['String']['output']>;
+  createdAt: Scalars['Float']['output'];
+  emotion?: Maybe<PersonaEmotion>;
+  id: Scalars['ID']['output'];
+  memoryIndex?: Maybe<PersonaMemoryIndex>;
+  meta?: Maybe<PersonaMeta>;
+  name?: Maybe<Scalars['String']['output']>;
+  persona?: Maybe<PersonaData>;
+  relationship?: Maybe<PersonaRelationship>;
+  settings?: Maybe<PersonaSettings>;
+  state?: Maybe<PersonaState>;
+  updatedAt: Scalars['Float']['output'];
+};
+
+export type PersonaData = {
+  __typename?: 'PersonaData';
+  archetype?: Maybe<Scalars['String']['output']>;
+  baseSystemPrompt?: Maybe<Scalars['String']['output']>;
+  coreTraits?: Maybe<Array<Maybe<Scalars['String']['output']>>>;
+  dynamicSystemPrompt?: Maybe<Scalars['String']['output']>;
+  tone?: Maybe<Scalars['String']['output']>;
+};
+
+export type PersonaEmotion = {
+  __typename?: 'PersonaEmotion';
+  current?: Maybe<Scalars['String']['output']>;
+  lastUpdated?: Maybe<Scalars['Float']['output']>;
+};
+
+export type PersonaMemoryIndex = {
+  __typename?: 'PersonaMemoryIndex';
+  longTermMemories?: Maybe<Array<Maybe<Scalars['String']['output']>>>;
+  shortTermSummary?: Maybe<Scalars['String']['output']>;
+};
+
+export type PersonaMeta = {
+  __typename?: 'PersonaMeta';
+  interactions?: Maybe<Scalars['Int']['output']>;
+  tokensUsed?: Maybe<Scalars['Int']['output']>;
+  version?: Maybe<Scalars['String']['output']>;
+};
+
+export type PersonaRelationship = {
+  __typename?: 'PersonaRelationship';
+  closeness?: Maybe<Scalars['Float']['output']>;
+  history?: Maybe<Array<Maybe<Scalars['String']['output']>>>;
+  trust?: Maybe<Scalars['Float']['output']>;
+};
+
+export type PersonaSettings = {
+  __typename?: 'PersonaSettings';
+  memoryRetention?: Maybe<Scalars['Float']['output']>;
+  model?: Maybe<Scalars['String']['output']>;
+  openness?: Maybe<Scalars['Float']['output']>;
+  temperature?: Maybe<Scalars['Float']['output']>;
+};
+
+export type PersonaState = {
+  __typename?: 'PersonaState';
+  attention?: Maybe<Scalars['Float']['output']>;
+  availability?: Maybe<Scalars['String']['output']>;
+  energy?: Maybe<Scalars['Float']['output']>;
+};
+
 export type Query = {
   __typename?: 'Query';
   chat?: Maybe<Chat>;
@@ -148,6 +231,8 @@ export type Query = {
   hello?: Maybe<Scalars['String']['output']>;
   message?: Maybe<Message>;
   messages?: Maybe<Array<Message>>;
+  persona?: Maybe<Persona>;
+  personas?: Maybe<Array<Persona>>;
   users?: Maybe<Array<User>>;
 };
 
@@ -176,6 +261,11 @@ export type QueryMessagesArgs = {
   chatId: Scalars['ID']['input'];
 };
 
+
+export type QueryPersonaArgs = {
+  id: Scalars['ID']['input'];
+};
+
 export type Subscription = {
   __typename?: 'Subscription';
   _empty?: Maybe<Scalars['String']['output']>;
@@ -185,6 +275,11 @@ export type Subscription = {
 
 export type SubscriptionNewMessageArgs = {
   chatId: Scalars['ID']['input'];
+};
+
+export type UpdatePersonaInput = {
+  avatarUrl?: InputMaybe<Scalars['String']['input']>;
+  name?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type User = {
@@ -278,15 +373,26 @@ export type ResolversTypes = {
   CreateChatWithMessageInput: CreateChatWithMessageInput;
   Float: ResolverTypeWrapper<Scalars['Float']['output']>;
   ID: ResolverTypeWrapper<Scalars['ID']['output']>;
+  Int: ResolverTypeWrapper<Scalars['Int']['output']>;
   JSON: ResolverTypeWrapper<Scalars['JSON']['output']>;
   Memory: ResolverTypeWrapper<Memory>;
   Message: ResolverTypeWrapper<Message>;
   MessageInput: MessageInput;
+  MessageMetadata: ResolverTypeWrapper<MessageMetadata>;
   MessageSenderEnum: MessageSenderEnum;
   Mutation: ResolverTypeWrapper<Record<PropertyKey, never>>;
+  Persona: ResolverTypeWrapper<Persona>;
+  PersonaData: ResolverTypeWrapper<PersonaData>;
+  PersonaEmotion: ResolverTypeWrapper<PersonaEmotion>;
+  PersonaMemoryIndex: ResolverTypeWrapper<PersonaMemoryIndex>;
+  PersonaMeta: ResolverTypeWrapper<PersonaMeta>;
+  PersonaRelationship: ResolverTypeWrapper<PersonaRelationship>;
+  PersonaSettings: ResolverTypeWrapper<PersonaSettings>;
+  PersonaState: ResolverTypeWrapper<PersonaState>;
   Query: ResolverTypeWrapper<Record<PropertyKey, never>>;
   String: ResolverTypeWrapper<Scalars['String']['output']>;
   Subscription: ResolverTypeWrapper<Record<PropertyKey, never>>;
+  UpdatePersonaInput: UpdatePersonaInput;
   User: ResolverTypeWrapper<User>;
 };
 
@@ -298,20 +404,33 @@ export type ResolversParentTypes = {
   CreateChatWithMessageInput: CreateChatWithMessageInput;
   Float: Scalars['Float']['output'];
   ID: Scalars['ID']['output'];
+  Int: Scalars['Int']['output'];
   JSON: Scalars['JSON']['output'];
   Memory: Memory;
   Message: Message;
   MessageInput: MessageInput;
+  MessageMetadata: MessageMetadata;
   Mutation: Record<PropertyKey, never>;
+  Persona: Persona;
+  PersonaData: PersonaData;
+  PersonaEmotion: PersonaEmotion;
+  PersonaMemoryIndex: PersonaMemoryIndex;
+  PersonaMeta: PersonaMeta;
+  PersonaRelationship: PersonaRelationship;
+  PersonaSettings: PersonaSettings;
+  PersonaState: PersonaState;
   Query: Record<PropertyKey, never>;
   String: Scalars['String']['output'];
   Subscription: Record<PropertyKey, never>;
+  UpdatePersonaInput: UpdatePersonaInput;
   User: User;
 };
 
 export type ChatResolvers<ContextType = any, ParentType extends ResolversParentTypes['Chat'] = ResolversParentTypes['Chat']> = {
   createdAt?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  personaId?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  summary?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   title?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   topic?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   updatedAt?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
@@ -333,11 +452,18 @@ export type MemoryResolvers<ContextType = any, ParentType extends ResolversParen
 export type MessageResolvers<ContextType = any, ParentType extends ResolversParentTypes['Message'] = ResolversParentTypes['Message']> = {
   chatId?: Resolver<Maybe<ResolversTypes['ID']>, ParentType, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  metadata?: Resolver<Maybe<ResolversTypes['JSON']>, ParentType, ContextType>;
+  metadata?: Resolver<Maybe<ResolversTypes['MessageMetadata']>, ParentType, ContextType>;
   sender?: Resolver<ResolversTypes['MessageSenderEnum'], ParentType, ContextType>;
   text?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   timestamp?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
   userId?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+};
+
+export type MessageMetadataResolvers<ContextType = any, ParentType extends ResolversParentTypes['MessageMetadata'] = ResolversParentTypes['MessageMetadata']> = {
+  emotion?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  intent?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  isMemoryWorthy?: Resolver<Maybe<ResolversTypes['Boolean']>, ParentType, ContextType>;
+  memoryTag?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
 };
 
 export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
@@ -349,8 +475,67 @@ export type MutationResolvers<ContextType = any, ParentType extends ResolversPar
   deleteMessage?: Resolver<ResolversTypes['Message'], ParentType, ContextType, RequireFields<MutationDeleteMessageArgs, 'id'>>;
   deleteUser?: Resolver<ResolversTypes['User'], ParentType, ContextType, RequireFields<MutationDeleteUserArgs, 'id'>>;
   updateMessage?: Resolver<ResolversTypes['Message'], ParentType, ContextType, RequireFields<MutationUpdateMessageArgs, 'id' | 'input'>>;
+  updatePersona?: Resolver<ResolversTypes['Persona'], ParentType, ContextType, RequireFields<MutationUpdatePersonaArgs, 'id' | 'input'>>;
   updateUser?: Resolver<ResolversTypes['User'], ParentType, ContextType, RequireFields<MutationUpdateUserArgs, 'id'>>;
   upsertUser?: Resolver<ResolversTypes['User'], ParentType, ContextType, RequireFields<MutationUpsertUserArgs, 'email' | 'firebaseUid'>>;
+};
+
+export type PersonaResolvers<ContextType = any, ParentType extends ResolversParentTypes['Persona'] = ResolversParentTypes['Persona']> = {
+  avatarUrl?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  createdAt?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
+  emotion?: Resolver<Maybe<ResolversTypes['PersonaEmotion']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  memoryIndex?: Resolver<Maybe<ResolversTypes['PersonaMemoryIndex']>, ParentType, ContextType>;
+  meta?: Resolver<Maybe<ResolversTypes['PersonaMeta']>, ParentType, ContextType>;
+  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  persona?: Resolver<Maybe<ResolversTypes['PersonaData']>, ParentType, ContextType>;
+  relationship?: Resolver<Maybe<ResolversTypes['PersonaRelationship']>, ParentType, ContextType>;
+  settings?: Resolver<Maybe<ResolversTypes['PersonaSettings']>, ParentType, ContextType>;
+  state?: Resolver<Maybe<ResolversTypes['PersonaState']>, ParentType, ContextType>;
+  updatedAt?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
+};
+
+export type PersonaDataResolvers<ContextType = any, ParentType extends ResolversParentTypes['PersonaData'] = ResolversParentTypes['PersonaData']> = {
+  archetype?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  baseSystemPrompt?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  coreTraits?: Resolver<Maybe<Array<Maybe<ResolversTypes['String']>>>, ParentType, ContextType>;
+  dynamicSystemPrompt?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  tone?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+};
+
+export type PersonaEmotionResolvers<ContextType = any, ParentType extends ResolversParentTypes['PersonaEmotion'] = ResolversParentTypes['PersonaEmotion']> = {
+  current?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  lastUpdated?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+};
+
+export type PersonaMemoryIndexResolvers<ContextType = any, ParentType extends ResolversParentTypes['PersonaMemoryIndex'] = ResolversParentTypes['PersonaMemoryIndex']> = {
+  longTermMemories?: Resolver<Maybe<Array<Maybe<ResolversTypes['String']>>>, ParentType, ContextType>;
+  shortTermSummary?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+};
+
+export type PersonaMetaResolvers<ContextType = any, ParentType extends ResolversParentTypes['PersonaMeta'] = ResolversParentTypes['PersonaMeta']> = {
+  interactions?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
+  tokensUsed?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
+  version?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+};
+
+export type PersonaRelationshipResolvers<ContextType = any, ParentType extends ResolversParentTypes['PersonaRelationship'] = ResolversParentTypes['PersonaRelationship']> = {
+  closeness?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+  history?: Resolver<Maybe<Array<Maybe<ResolversTypes['String']>>>, ParentType, ContextType>;
+  trust?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+};
+
+export type PersonaSettingsResolvers<ContextType = any, ParentType extends ResolversParentTypes['PersonaSettings'] = ResolversParentTypes['PersonaSettings']> = {
+  memoryRetention?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+  model?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  openness?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+  temperature?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+};
+
+export type PersonaStateResolvers<ContextType = any, ParentType extends ResolversParentTypes['PersonaState'] = ResolversParentTypes['PersonaState']> = {
+  attention?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
+  availability?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  energy?: Resolver<Maybe<ResolversTypes['Float']>, ParentType, ContextType>;
 };
 
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
@@ -361,6 +546,8 @@ export type QueryResolvers<ContextType = any, ParentType extends ResolversParent
   hello?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType, Partial<QueryHelloArgs>>;
   message?: Resolver<Maybe<ResolversTypes['Message']>, ParentType, ContextType, RequireFields<QueryMessageArgs, 'id'>>;
   messages?: Resolver<Maybe<Array<ResolversTypes['Message']>>, ParentType, ContextType, RequireFields<QueryMessagesArgs, 'chatId'>>;
+  persona?: Resolver<Maybe<ResolversTypes['Persona']>, ParentType, ContextType, RequireFields<QueryPersonaArgs, 'id'>>;
+  personas?: Resolver<Maybe<Array<ResolversTypes['Persona']>>, ParentType, ContextType>;
   users?: Resolver<Maybe<Array<ResolversTypes['User']>>, ParentType, ContextType>;
 };
 
@@ -385,7 +572,16 @@ export type Resolvers<ContextType = any> = {
   JSON?: GraphQLScalarType;
   Memory?: MemoryResolvers<ContextType>;
   Message?: MessageResolvers<ContextType>;
+  MessageMetadata?: MessageMetadataResolvers<ContextType>;
   Mutation?: MutationResolvers<ContextType>;
+  Persona?: PersonaResolvers<ContextType>;
+  PersonaData?: PersonaDataResolvers<ContextType>;
+  PersonaEmotion?: PersonaEmotionResolvers<ContextType>;
+  PersonaMemoryIndex?: PersonaMemoryIndexResolvers<ContextType>;
+  PersonaMeta?: PersonaMetaResolvers<ContextType>;
+  PersonaRelationship?: PersonaRelationshipResolvers<ContextType>;
+  PersonaSettings?: PersonaSettingsResolvers<ContextType>;
+  PersonaState?: PersonaStateResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
   Subscription?: SubscriptionResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
