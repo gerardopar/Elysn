d7ee783423127c5fece24e03720ba30a8e0c2e9e
Gerardo Paredes <gerardparedes23@gmail.com>
2025-12-11 23:45:13 -0800

    add extractUserSignal

--- apps/server/src/helpers/interlink.helpers.ts
+++ apps/server/src/helpers/interlink.helpers.ts
@@ -1,4 +1,10 @@
+import { openaiClient as openai } from "../services/openAi.js";
+import { OpenAI } from "openai";
+
 import { getInterlink, createInterlink } from "../access-layer/interlink.js";
+
+import { userSignalResponse } from "@elysn/core";
+import { sanitizeJSON } from "./string.helpers.js";
 
 export const getOrCreateInterlink = async (
   userId: string,
@@ -9,4 +15,39 @@ export const getOrCreateInterlink = async (
 
   const interlink = await createInterlink(userId, personaId);
   return interlink;
+};
+
+export const extractUserSignal = async (
+  messageText: string
+): Promise<{
+  sentiment: number;
+  emotion: string;
+  intensity: number;
+} | null> => {
+  if (!messageText) return null;
+
+  const payload = userSignalResponse(messageText);
+
+  if (!payload) return null;
+
+  const response: OpenAI.Responses.Response = await openai.responses.create(
+    payload
+  );
+
+  if (!response || !response.output_text) return null;
+  const sanitizedOutput = sanitizeJSON(response.output_text);
+  const extractedTopics: {
+    sentiment: number;
+    emotion: string;
+    intensity: number;
+  } = JSON.parse(sanitizedOutput);
+
+  if (
+    !extractedTopics?.sentiment ||
+    !extractedTopics?.emotion ||
+    !extractedTopics?.intensity
+  )
+    return null;
+
+  return extractedTopics;
 };
--- apps/server/src/helpers/message.helpers.ts
+++ apps/server/src/helpers/message.helpers.ts
@@ -1,8 +1,17 @@
 import { Message } from "../models/message.js";
 
+import { MessageMetadata } from "@elysn/core";
+
 export const updateMessageEmbedding = async (
   messageId: string,
   embedding: number[] | null
 ): Promise<void> => {
   await Message.updateOne({ _id: messageId }, { metadata: { embedding } });
+};
+
+export const updateMessageMetadata = async (
+  messageId: string,
+  metadata: Partial<MessageMetadata>
+): Promise<void> => {
+  await Message.updateOne({ _id: messageId }, { metadata });
 };
